1a7d5a5ac2944a4fb4cf816d14aedf2c0b58431d
Paul Brennan <paul.brennan@meastim.com>
2021-10-02 19:16:59 +0100

    Login system

--- authService.py
+++ authService.py
@@ -1,11 +1,24 @@
 from db import db
 from flask_bcrypt import Bcrypt
+from user import user
 class authService:
 
     def __init__(self, app):
         if(not hasattr(self, 'conn')):
             self.db = db(app)
         self.bcrypt = Bcrypt(app)
+
+    def get_user_by_id(self, id):
+        cursor = self.db.conn.cursor()
+        cursor.execute("SELECT * FROM users WHERE id = %s LIMIT 1", (id))
+        if(cursor.rowcount > 0):
+            data = cursor.fetchone()
+            data = user(data[0], data[1], data[2])
+        else:
+            data = False
+        cursor.close()
+        return data
+
 
     def create_user(self, username, password):
         pw_hash = self.bcrypt.generate_password_hash(password).decode('utf-8')
@@ -14,5 +27,31 @@ class authService:
         self.db.conn.commit()
         cursor.close()
     
+    def select_user(self, username):
+        cursor = self.db.conn.cursor()
+        cursor.execute("SELECT * FROM users WHERE username = %s LIMIT 1", (username))
+        if(cursor.rowcount > 0):
+            data = cursor.fetchone()
+            data = user(data[0], data[1], data[2])
+        else:
+            data = False
+        cursor.close()
+        return data
+
+    def check_password_hash(self, password, passwordHash):
+        return self.bcrypt.check_password_hash(passwordHash, password)
+
+    def login(self, username, password):
+        selectedUserData = self.select_user(username)
+        if(selectedUserData != False):
+            if(self.check_password_hash(password, selectedUserData.password)):
+                return selectedUserData
+            else:
+                return False
+        return False
+        
+
+    
+
 
 
--- main.py
+++ main.py
@@ -1,19 +1,72 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for, request, flash
+from flask.wrappers import Response
 from flaskext.mysql import MySQL
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+from flask import session
+from flask_login import login_user, logout_user, login_required
 from db import db
 from authService import authService
 
 app = Flask(__name__)
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 app.config['MYSQL_DATABASE_USER'] = 'root'
 #app.config['MYSQL_DATABASE_PASSWORD'] = ''
 app.config['MYSQL_DATABASE_DB'] = 'nasa_lunar'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 
+login_manager = LoginManager()
+login_manager.init_app(app)
+
 dbConnection = db(app)
 authService = authService(app)
 
 authService.create_user('admin', 'admin')
+authService.select_user('admin')
+#loginResp = authService.login('admin', 'admin')
+#if(loginResp != False):
+#    login_user(loginResp)
 @app.route('/')
 def hello():
     return 'Hello, World!'
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    
+@app.route('/profile')
+@login_required
+def profile():
+    return "Logged in - profile"
+
+@app.route('/login', methods=['POST'])
+def login_post():
+    username = request.form.get('username')
+    password = request.form.get('password')
+
+    loginResp = authService.login(username, password)
+    if(loginResp != False):
+        login_user(loginResp)
+        return ("Logged in")
+    else:
+        return "Login failed"
+
+@app.route('/signup', methods=['POST'])
+def signup_post():
+    username = request.form.get('username')
+    password = request.form.get('password')
+
+    loginResp = authService.select_user(username)
+    if(loginResp == False):
+        authService.create_user(username, password)
+        flash("Successfully created the user, you can now sign in!")
+    else:
+        flash("Username is already taken")
+
+@login_manager.user_loader
+def load_user(user_id):
+    resp = authService.get_user_by_id(user_id)
+    if(resp != False):
+        return resp
+    return None
