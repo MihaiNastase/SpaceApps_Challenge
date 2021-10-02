8e881286e9649b9dea04128fc37a39ae193e56d8
Mihai Nastase <Mihai.Valentin.Nastase@outlook.com>
2021-10-02 15:43:28 +0100

    Barebones Flask-React connection done

---
+++ client/src/App.js
@@ -0,0 +1,43 @@
+import logo from './logo.svg';
+import './App.css';
+import {useState, useEffect} from 'react';
+
+function App() {
+
+  const[logs, setLogs] = useState([]);
+
+  useState(() => {
+    fetch('http://localhost:5000/', {
+      'methods':'GET',
+      headers: {
+        'Content-Type':'application/json'
+      }
+    })
+    .then(resp => resp.json())
+    .then(resp => console.log(resp))
+    .then(resp => setLogs(resp))
+    .catch(error => console.log(error))
+  },[]);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Development build, work still in progress...
+        </p>
+        <a
+          className="App-link"
+          href="https://2021.spaceappschallenge.org/challenges/statements/lunar-surface-operations-real-time-collaboration/teams/rhuston-we-have-a-problem/project"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          r/huston-we-have-a-problem
+        </a>
+      </header>
+
+    </div>
+  );
+}
+
+export default App;
