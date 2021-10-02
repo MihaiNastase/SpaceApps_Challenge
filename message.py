561355e70d7f184fae63589cc87865bbb6483280
Paul Brennan <paulpatrickbrennan2002@gmail.com>
2021-10-02 13:58:14 +0100

    Simple chat

---
+++ message.py
@@ -0,0 +1,6 @@
+class Message:
+    def __init__(self, messageId, messageText, userId, creationDatetime):
+        self.messageId = messageId
+        self.messageText = messageText
+        self.userId = userId
+        self.creationDatetime = creationDatetime
