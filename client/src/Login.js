3dfd307b0bec82c4159a18e98940e3a17abe6120
MihaiNastase <Mihai.Nastase@mail.bcu.ac.uk>
2021-10-03 20:32:24 +0100

    fuckall :

--- client/src/Login.js
+++ client/src/Login.js
@@ -1,22 +1,71 @@
-import React from 'react';
+import React, {useState} from 'react';
+import axios from "axios";
 import './CRT.css'
 
 export default function Login() {
-  return(
-    <div className="login-wrapper">
-   <form>
-      <label class="crt site-title">
-        <p>Username</p>
-        <input type="text" />
-      </label>
-      <label class="crt site-title">
-        <p>Access key</p>
-        <input type="password" />
-      </label>
-      <div>
-        <button type="submit">Access</button>
-      </div>
-    </form>
-    </div>
-  );
+        const [state , setState] = useState({
+            username : "",
+            password : ""
+        })
+        //When 'submit' clicked, send details to API
+        const handleSubmitClick = (e) => {
+            e.preventDefault();
+            console.log(state.password)
+            sendDetails()
+        }
+        //Handle inputs into input fields
+        const handleChange = (e) => {
+            const {id , value} = e.target
+            setState(prevState => ({
+                ...prevState,
+                [id] : value
+            }))
+        }
+        //JSONify the username and password
+        const sendDetails = () => {
+    
+            const body = {
+                "username":state.username,
+                "password":state.password,
+            }
+    
+            axios.post('http://localhost:5000/login', body)
+                .then((response)=>{
+                     if(response.status === 200){
+                            if(response.data.status) {
+                                return(alert("Access Granted!"));
+                            }
+                            else {
+                                return(alert("Permission Denied!"));
+                            }
+                }
+                
+            })
+        }
+
+    return(
+    <div className="crt login-wrapper">
+    <form>
+        <label class="site-title" htmlFor="userId">
+            <p>Username</p>
+            <input type="string" 
+                id="username"
+                value={state.username}
+                onChange={handleChange}
+            />
+        </label>
+        <label class="site-title">
+            <p>Access key</p>
+            <input type="password"
+            value={state.password}               
+            onChange={handleChange}
+       />
+        </label>
+        <div>
+            <button class="site-title" type="submit" onClick={handleSubmitClick}>Access</button>
+        </div>
+        </form>
+        </div>
+    
+  )
 }
